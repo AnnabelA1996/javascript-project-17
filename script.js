f1503b0264178edaebd221ba0b7cc2ceca89bcac
Annabel Ambrosius <annabelambrosius@Annabels-MacBook-Air.local>
2020-12-01 19:52:50 +1300

    git

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-document.querySelector('.tab').addEventListener('click' , function(){
-var panel = document.querySelector('.panel')
-var icon = document.querySelector('i')
-var text = document.querySelector('span')
-if(icon.classList.contains('fa-arrow-down')) {
-panel.style.height = '200px'
-icon.classList.remove('fa-arrow-down')
-icon.classList.add('fa-arrow-up')
-text.innerHTML = 'close'
-}else{
-panel.style.height = '0'
-icon.classList.replace('fa-arrow-up' , 'fa-arrow-down')
-text.innerHTML = 'open'
-}
+var section = document.querySelector('section')
 
+var p = document.createElement('p')
+p.innerHTML = 'i just created this element'
+section.append(p)
+
+var names = ['john', 'mary', 'tom', 'steve']
+var numbers = ['1', '2', '3']
+var ul = document.createElement('ul')
+document.body.appendChild(ul)
+
+names.forEach(function (name){
+   var li = document.createElement('li')
+   li.innerHTML =  name
+   ul.append(li)
 })
+
+function add() {
+    var text = document.querySelector('[name="text"').value
+    // var ol = document.createElement('ol')
+    // document.body.appendChild(ol)
+    ol = document.querySelector ('ol')
+    var li = document.createElement('li')
+    li.innerHTML = text
+    ol.append(li)
+    document.querySelector('[name="text"').value =''
+}
